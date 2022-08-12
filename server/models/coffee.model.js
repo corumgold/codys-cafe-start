3e25ff92e4234cbaeabc8b4d6ddb4135f4fa5a10
Corum Gold <goldcorum@gmail.com>
2022-08-12 11:43:03 -0400

    Finish Coffee

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,35 @@
-const Sequelize = require('sequelize')
-const db = require('./database')
+const { Sequelize, Op } = require("sequelize");
+const db = require("./database");
 
-const Coffee = db.define('coffee', {
-  // your code here
-})
+const Coffee = db.define("coffee", {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  ingredients: {
+    type: Sequelize.ARRAY(Sequelize.DataTypes.STRING),
+  },
+});
 
-module.exports = Coffee
+Coffee.prototype.getIngredients = function () {
+  return this.ingredients.join(", ");
+};
+
+Coffee.findByIngredient = async function (ingredient) {
+  let foundCoffee = await Coffee.findAll({
+    where: {
+      ingredients: {
+        [Op.contains]: [ingredient],
+      },
+    },
+  });
+  return foundCoffee;
+};
+
+Coffee.beforeValidate((coffee) => {
+  if (!coffee.ingredients.includes("love")) {
+    coffee.ingredients.push("love");
+  }
+});
+
+module.exports = Coffee;
