459e76608a5d901e3c252bb4bc2b32d773a50594
Benjamin Lee <benjaminlee0727@gmail.com>
2022-08-12 12:10:03 -0500

    Finish pug model

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,9 @@ const Pug = require('./pug.model')
 const Coffee = require('./coffee.model')
 
 // VVV assign relations below VVV //
-
+Pug.belongsTo(Coffee, {as : 'favoriteCoffee'});
+Coffee.hasMany(Pug);
+Pug.hasMany(Pug, {as: 'friends'})
 
 // ^^^ assign relations above ^^^ //
 
@@ -12,3 +14,19 @@ module.exports = {
   Pug,
   Coffee
 }
+
+// Page.belongsTo(User, { as: "author" });
+// User.hasMany(Page, {foreignKey: 'authorId'});
+// Page.belongsToMany(Tag, {through: 'page_tags'});
+// Tag.belongsToMany(Page, {through: 'page_tags'});
+
+//one to one : a person has a best friend => use belongsTo
+
+// one to many: a teacher has many students
+//teacher hasMany Students
+//student belongsTo Teacher; student has foreign key
+
+
+// many to many : a student can have many mentors, mentors can have many students
+// only use belongsToMany
+// needs a through table, defined on both 
--- server/models/pug.model.js
+++ server/models/pug.model.js
@@ -2,8 +2,18 @@ const Sequelize = require('sequelize')
 const db = require('./database')
 const Coffee = require('./coffee.model')
 
-const Pug = db.define('pugs', {
-  // your code here
+const Pug = db.define('pug', {
+  name : {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  age : {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+  biography : {
+    type: Sequelize.TEXT
+  }
 })
 
 module.exports = Pug
